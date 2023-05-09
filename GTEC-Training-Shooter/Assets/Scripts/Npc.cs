bd721719d917644c8badd0095c868a2996304452
Mr.Fumpledump <Fumpledump@gmail.com>
2023-05-09 11:27:00 -0400

    Effects Added

--- GTEC-Training-Shooter/Assets/Scripts/Npc.cs
+++ GTEC-Training-Shooter/Assets/Scripts/Npc.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Npc : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class Npc : MonoBehaviour
 
     public Material flashMaterial;
     public Material darkMaterial;
+
+    public ParticleSystem explosionEffect;
 
     private void Start()
     {
@@ -38,6 +41,11 @@ public class Npc : MonoBehaviour
             Debug.Log("Civilian Shot!");
             gameHandler.IncreaseScore(-100);
         }
+
+        // Play Explosion Effect
+        ParticleSystem newHitEffect = Instantiate(explosionEffect, transform.position, transform.rotation);
+        newHitEffect.Play();
+        Destroy(newHitEffect.gameObject, 1.5f);
 
         // Deactivate instead of destroy so that EEG handler does not lose track of the object
         // Game object will be destroyed when GameHandler runs CleanUpTargets()
